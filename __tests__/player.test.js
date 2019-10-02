54fbeecea9b65be882532d7b9bffdca3b4d21152
paramagicdev <konnor5456@gmail.com>
2019-10-02 15:17:49 -0400

    fully configured jest and everything works as intended currently

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -4,6 +4,7 @@ test('creates a blank player', () => {
   const blank = Player();
   expect(blank.name).toBe(undefined);
 });
+
 test("Creates a player with name 'test dummy'", () => {
   const testDummy = Player({name: 'test dummy'});
   expect(testDummy.name).toBe('test dummy');
